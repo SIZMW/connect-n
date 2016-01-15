0ab041114f2a728f4388e435ed972168d2bd0ba1
Daniel Beckwith <djbsnx@gmail.com>
2016-01-15 16:36:21 -0500

    Added testing framework

---
+++ src/test/java/edu/wpi/cs/connectn/GameTreeGeneratorTest.java
@@ -0,0 +1,13 @@
+package edu.wpi.cs.connectn;
+
+import org.junit.Test;
+
+/**
+ * Created by djbsn_000 on 1/15/2016.
+ */
+public class GameTreeGeneratorTest {
+
+    @Test
+    public void testGenerateChildren() throws Exception {
+    }
+}
