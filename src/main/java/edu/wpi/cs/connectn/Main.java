ad912fa1d5d6810e4b9e7d6b885d10a1ad7b4356
Daniel Beckwith <djbsnx@gmail.com>
2016-01-15 14:22:10 -0500

    Initial commit.

---
+++ src/main/java/edu/wpi/cs/connectn/Main.java
@@ -0,0 +1,11 @@
+package edu.wpi.cs.connectn;
+
+/**
+ * Created by djbsn_000 on 1/15/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
