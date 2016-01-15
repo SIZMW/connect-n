b2f0a8dc397d804f04617dd735b69db457cb6de9
Aditya Nivarthi <adityanivarthi19@gmail.com>
2016-01-15 15:10:10 -0500

    Added GameState and Player classes.

---
+++ src/main/java/edu/wpi/cs/connectn/GameState.java
@@ -0,0 +1,10 @@
+package edu.wpi.cs.connectn;
+
+
+public class GameState {
+    Player[][] boardState;
+
+    public GameState() {
+
+    }
+}
---
+++ src/main/java/edu/wpi/cs/connectn/Player.java
@@ -0,0 +1,5 @@
+package edu.wpi.cs.connectn;
+
+public enum Player {
+    MIN, MAX
+}
