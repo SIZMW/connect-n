6e40a07d3ad50e25583aaafee4545d46a18a241b
Aditya <adityanivarthi19@gmail.com>
2016-01-17 11:37:25 -0500

    Updated comments in Move.java

--- src/main/java/edu/wpi/cs/connectn/Move.java
+++ src/main/java/edu/wpi/cs/connectn/Move.java
@@ -1,23 +1,44 @@
 package edu.wpi.cs.connectn;
 
+/**
+ * This class represents a move in a specified column of the Connect-N board.
+ * @author Aditya Nivarthi
+ */
 public class Move {
 
     private final MoveType type;
     private final int column;
 
+    /**
+     * Creates a Move instance with the specified type and column.
+     * @param type The type of move, POP or DROP.
+     * @param column The column where the move is being executed.
+     */
     public Move(MoveType type, int column) {
         this.type = type;
         this.column = column;
     }
 
+    /**
+     * Returns the move type of this move.
+     * @return a MoveType.
+     */
     public MoveType getType() {
         return type;
     }
 
+    /**
+     * Returns the column of this move.
+     * @return an integer
+     */
     public int getColumn() {
         return column;
     }
 
+    /**
+     * Returns a string representation of this object.
+     * @return a String
+     */
     @Override
     public String toString() {
         return type + " move in column " + column;
