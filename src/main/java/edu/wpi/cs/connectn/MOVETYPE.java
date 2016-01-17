6c23816925314a14cb82c757fa895947ce9eebaf
Aditya <adityanivarthi19@gmail.com>
2016-01-17 12:22:23 -0500

    Updated comments in MoveType.java.

--- src/main/java/edu/wpi/cs/connectn/MOVETYPE.java
+++ src/main/java/edu/wpi/cs/connectn/MOVETYPE.java
@@ -1,5 +1,10 @@
 package edu.wpi.cs.connectn;
 
+/**
+ * This enumerated type represents the types of moves that can be applied to a {@Link GameState}.
+ *
+ * @author Daniel Beckwith
+ */
 public enum MoveType {
     DROP, POP
 }
