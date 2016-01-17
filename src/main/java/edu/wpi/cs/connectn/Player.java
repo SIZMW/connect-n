b1f670ac0a6a094ab5195c7eba22bcec8c28fc96
Aditya <adityanivarthi19@gmail.com>
2016-01-17 12:20:21 -0500

    Updated comments in Player.java

--- src/main/java/edu/wpi/cs/connectn/Player.java
+++ src/main/java/edu/wpi/cs/connectn/Player.java
@@ -1,12 +1,27 @@
 package edu.wpi.cs.connectn;
 
+/**
+ * This enumerated type represents the players that can be on the game board at any time in the game.
+ *
+ * @author Aditya Nivarthi
+ */
 public enum Player {
     MIN, MAX;
 
+    /**
+     * Returns this Player in the form of a {@Link BoardCell}.
+     *
+     * @return a {@Link BoardCell}
+     */
     public BoardCell getAsBoardCell() {
         return BoardCell.valueOf(this.toString());
     }
 
+    /**
+     * Returns this Player in the form of a {@Link GameWinner}.
+     *
+     * @return a {@Link GameWinner}
+     */
     public GameWinner getAsGameWinner() {
         return GameWinner.valueOf(this.toString());
     }
