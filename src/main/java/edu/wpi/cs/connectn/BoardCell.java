bff8fd1b8397b376ae21db8db5cc3227d8c7f396
Aditya <adityanivarthi19@gmail.com>
2016-01-17 12:21:00 -0500

    Updated comments in BoardCell.java.

--- src/main/java/edu/wpi/cs/connectn/BoardCell.java
+++ src/main/java/edu/wpi/cs/connectn/BoardCell.java
@@ -1,5 +1,10 @@
 package edu.wpi.cs.connectn;
 
+/**
+ * This enumerated type represents the states that can be in any position of the Connect-N board.
+ *
+ * @author Aditya Nivarthi
+ */
 public enum BoardCell {
     MIN("-"),
     MAX("+"),
@@ -7,14 +12,29 @@ public enum BoardCell {
 
     private String symbol;
 
+    /**
+     * Creates a BoardCell instance with the specified symbol.
+     *
+     * @param symbol The symbol to represent the enumerated type in string form.
+     */
     BoardCell(String symbol) {
         this.symbol = symbol;
     }
 
+    /**
+     * Returns the symbol for this enumerated type.
+     *
+     * @return a String
+     */
     public String getSymbol() {
         return symbol;
     }
 
+    /**
+     * Returns this enumerated type in the form of a {@Link GameWinner} enumerated type.
+     *
+     * @return a {@Link GameWinner}
+     */
     public GameWinner getAsGameWinner() {
         return GameWinner.valueOf(this.toString());
     }
