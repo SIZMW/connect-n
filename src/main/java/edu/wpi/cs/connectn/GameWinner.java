89b841e9e5eab4f721128c8283be8a80ce731f02
Aditya <adityanivarthi19@gmail.com>
2016-01-17 12:21:50 -0500

    Updated Comments in GameWinner.java

--- src/main/java/edu/wpi/cs/connectn/GameWinner.java
+++ src/main/java/edu/wpi/cs/connectn/GameWinner.java
@@ -1,5 +1,10 @@
 package edu.wpi.cs.connectn;
 
+/**
+ * This enumerated type represents the potential winners of any given {@Link GameState}.
+ *
+ * @author Aditya Nivarthi
+ */
 public enum GameWinner {
     MIN, MAX, TIE, NONE
 }
